dd05c4ab90c6ca8237c423c1293d9d403d8dc4a2
ItzCinder <jeanlacruz14@hotmail.com>
2026-07-25 00:26:19 -0300

    Se creo el menu de administración de usuarios

---
+++ scripts/menus/users_menu.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Ruta de la carpeta donde se ubican los scripts
+export SCRIPTS_DIR="./scripts"
+while true; do
+    clear
+    echo "==============================================="
+    echo "      PANEL DE ADMINISTRACIÓN DE USUARIOS      "
+    echo "                 DEL SERVIDOR                  "
+    echo "==============================================="
+    echo
+    echo "1) Crear usuario"
+    echo "2) Editar usuario"
+    echo "3) Borrar usuario"
+    echo "0) Volver al menu principal"
+    echo
+    echo "==============================================="
+    read -p "Selecciona una opción: " option
+    case "$option" in
+        1)  # Crear usuario
+            bash "$SCRIPTS_DIR/users/create_user.sh"
+            ;;
+        2)  # Editar usuario
+            bash "$SCRIPTS_DIR/users/edit_user.sh"
+            ;;    
+        3)  # Borrar usuario
+            bash "$SCRIPTS_DIR/users/remove_user.sh"
+            ;;
+        0)  # Salir del menu
+            echo "Saliendo del panel..."
+            exit 0
+            ;;
+        *)  # Validación de entrada no existente
+            echo "Opcion no valida. Intenta de nuevo."
+            ;;
+    esac
+
+    echo ""
+    read -p "Presiona [Enter] para continuar..."
+done
