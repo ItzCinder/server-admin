0d93fb4fef9c037f4fd39824cd66124b6b9727ea
ItzCinder <jeanlacruz14@hotmail.com>
2026-07-25 00:09:40 -0300

    Actualización de comentarios

--- scripts/query/query_groups.sh
+++ scripts/query/query_groups.sh
@@ -7,7 +7,7 @@ while true; do
     echo "==============================================="
     echo "      LISTA DE GRUPOS DEL SERVIDOR             "
     echo "==============================================="
-    # Mostrar grupos
+    # Mostrar grupos sin incluir los del sistema
     awk -F: '$3 >= 1000 && $3 < 60000 { gsub(/,/, " ", $4); print "Grupo: " $1 " (GID: " $3 ") - Miembros: " $4 }' /etc/group || || echo "Hubo un error en la ejecución del comando"
     echo "==============================================="
     exit 0
